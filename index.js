7f5cf5c925792afa817aa67c6873977400b88d14
ClementPeriard <84712190+ClementPeriard@users.noreply.github.com>
2022-09-04 18:16:38 +0200

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function(){
-    var title = document.getElementById("title-1");
-    title.style = "font-size: 50px; transition: all 1s ease 0s; color: white;  text-shadow: 0 0 10px #283E51;";
- },50);
-
