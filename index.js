359f85a3418512c41c9895bb221261841b1185f5
ClementPeriard <84712190+ClementPeriard@users.noreply.github.com>
2022-11-13 18:48:49 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-setTimeout(function(){
-    var title = document.getElementById("title-1");
-    title.style = "font-size: 50px; transition: all 1s ease 0s; color: white;  text-shadow: 0 0 10px #283E51;";
- },50);
-
-function trial() {
-    window.open('http://www.europeangoldfinch.ml/check.html');
-}
